cd3de7b829f52b950e50a608dd80044853dedaa1
Jean Luiz <zatzk@outlook.com>
2022-10-21 19:50:58 -0300

    🚧 update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
+import { connectDatabase } from "./database/connectDatabase";
 
 (async () => {
   const BOT = new Client({intents: IntentOptions});
   
+  BOT.on("ready", () => console.log("Connected to Discord! 🎉"))
+
+
+
+
+
+  await connectDatabase()
   await BOT.login(process.env.BOT_TOKEN)
 })();
