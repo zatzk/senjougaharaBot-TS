4224330a36fed5074d8cef2604db9329a7ea8ad2
Jean Luiz <zatzk@outlook.com>
2022-10-21 20:19:56 -0300

    🚧 import validateEnv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
 import { connectDatabase } from "./database/connectDatabase";
+import { validateEnv } from "./utils/validateEnv";
 
 (async () => {
+  if(!validateEnv()) return
   const BOT = new Client({intents: IntentOptions});
   
   BOT.on("ready", () => console.log("Connected to Discord! 🎉"))
