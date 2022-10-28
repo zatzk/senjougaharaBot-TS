14da675f92007696832cc822e741bf975a5de5e9
Jean Luiz <zatzk@outlook.com>
2022-10-27 21:45:26 -0300

    👷 update index

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
-import { connectDatabase } from "./database/connectDatabase";
+import { onInteraction } from "./events/onInteraction";
+import { onReady } from "./events/onReady";
 import { validateEnv } from "./utils/validateEnv";
 
 (async () => {
   if(!validateEnv()) return
-  const BOT = new Client({intents: IntentOptions});
+  const client = new Client({intents: IntentOptions});
   
-  BOT.on("ready", () => console.log("Connected to Discord! 🎉"))
+  client.on("ready", async () => await onReady(client));
+  client.on(
+    "interactionCreate",
+    async (interaction) => await onInteraction(interaction)
+  )
 
-
-
-
-
-  await connectDatabase()
-  await BOT.login(process.env.BOT_TOKEN)
+  await client.login(process.env.BOT_TOKEN)
 })();
