33004f06393362badab0f866579ec805b7222b8f
Jean Luiz <zatzk@outlook.com>
2022-10-27 21:43:59 -0300

    🔨 added new files

---
+++ src/events/onReady.ts
@@ -0,0 +1,20 @@
+import { Client, REST, Routes } from "discord.js";
+import { CommandList } from "../commands/_CommandList";
+
+export const onReady = async (client: Client) => {
+  const rest = new REST({version: "9"}).setToken(
+    process.env.BOT_TOKEN as string
+  );
+
+  const commandData = CommandList.map((command) => command.data.toJSON());
+
+  await rest.put(
+    Routes.applicationGuildCommands(
+      client.user?.id || "missing id",
+      process.env.GUILD_ID as string
+    ),
+    {body: commandData}
+  );
+
+  console.log("Discord ready!")
+};
