128b4f2e0b47abe9ad2a96e7537541e11fccc084
Jean Luiz <zatzk@outlook.com>
2022-10-27 21:38:34 -0300

    ➖ removed mongoose lib

--- src/database/connectDatabase.ts
+++
@@ -1,7 +0,0 @@
-import { connect } from "mongoose";
-
-export const connectDatabase = async () => {
-    await connect(process.env.MONGO_URI as string);
-
-    console.log("Database Connected!")
-}
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -3,9 +3,5 @@ export const validateEnv = () => {
     console.warn("Missing Discord bot token.");
     return false;
   }
-  if(!process.env.MONGO_URI) {
-    console.warn("Missing MongoDB connection.");
-    return false;
-  }
   return true;
 }
