38e317a803e43b805a947c02fb3a1d974aea278d
Jean Luiz <zatzk@outlook.com>
2022-10-29 18:42:56 -0300

    🍱 update version

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -2,16 +2,25 @@ import { Client, REST, Routes } from "discord.js";
 import { CommandList } from "../commands/_CommandList";
 
 export const onReady = async (client: Client) => {
-  const rest = new REST({version: "9"}).setToken(
+  const rest = new REST({version: "10"}).setToken(
     process.env.BOT_TOKEN as string
   );
 
   const commandData = CommandList.map((command) => command.data.toJSON());
 
+  // GUILD 
+  // await rest.put(
+  //   Routes.applicationGuildCommands(
+  //     client.user?.id || "missing id",
+  //     process.env.GUILD_ID as string
+  //   ),
+  //   {body: commandData}
+  // );
+
+  //GLOBAL
   await rest.put(
-    Routes.applicationGuildCommands(
+    Routes.applicationCommands(
       client.user?.id || "missing id",
-      process.env.GUILD_ID as string
     ),
     {body: commandData}
   );
